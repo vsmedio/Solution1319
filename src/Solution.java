2c1be3d744f41d037d610935d8f8484cd82731e7
Arvis Abromovics <info@vsmedio.lv>
2018-01-10 20:30:49 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+/* 
+Писатель в файл с консоли
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку "exit".
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
+
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Создай и используй объект типа BufferedWriter.
+3. Программа не должна ничего читать из файловой системы.
+4. Программа должна читывать строки с консоли, пока пользователь не введет строку "exit".
+5. Программа должна записать абсолютно все введенные строки (включая "exit") в файл, каждую строчку с новой строки.
+6. Метод main должен закрывать объект типа BufferedWriter после использования.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String filename = reader.readLine();
+
+        BufferedWriter file = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename)));
+
+        String s = "";
+        while (!s.equals("exit")) {
+            s = reader.readLine();
+            file.write(s);
+            file.newLine();
+        }
+
+        file.close();
+        reader.close();
+    }
+}
